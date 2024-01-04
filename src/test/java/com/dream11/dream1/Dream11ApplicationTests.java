5262c627c9033b3c50dd65891a7bad593b81c798
Atit Patel <atitpatel296@gmail.com>
2024-01-04 10:27:11 +0530

    127:create save post feature

---
+++ src/test/java/com/dream11/dream1/Dream11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream11.dream1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dream11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
