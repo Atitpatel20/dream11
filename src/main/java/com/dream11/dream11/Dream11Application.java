6ac4aa8cf905e2163530ae2e6a8b8e2d347e8c14
Atit Patel <atitpatel296@gmail.com>
2024-01-04 11:13:47 +0530

    127:create save post feature add Entity

--- src/main/java/com/dream11/dream11/Dream11Application.java
+++ src/main/java/com/dream11/dream11/Dream11Application.java
@@ -1,4 +1,4 @@
-package com.dream11.dream1;
+package com.dream11.dream11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/dream11/dream11/entity/User.java
@@ -0,0 +1,21 @@
+package com.dream11.dream11.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="players")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private long mobile;
+}
